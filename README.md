10dfe636fcb3df72e142f403014f51c3f2270fec
Matthew Wurzberger <matt.wurzberger@gmail.com>
2014-07-19 05:34:00 -0400

    Finished setup as an npm module, got test frame configured, added prior written code to project

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ backbone-enhanced
 =================
 
 Adding additional properties to backbone. Such as auto generating getter/setter methods for attributes and subcollections.
+
+
+Auto-generate accessor methods for model attributes.
+
+Support attribute names int he following formats:
+> "attribute name"
+> "attribute-name"
+> "attribute_name"
+> "attribute.name"
+> "attributeName"
+
+Example:
+Attribute name of {"shots-remaining": 10}
+Getter .getShotsRemaining();
+Seetter .setShotsRemaining( param );
+
+Attribute name of {"clip-empty": false}
+Getter .isClipEmpty();
+Setter .setClipEmpty( param );
+
+
+
+TODO
+----
+Support for alternate boolean prefixes such as hasLicense(), canEvaluate(), and shouldHalt()
---
+++ test/main.js
@@ -0,0 +1,55 @@
+// test/main.js
+'use strict';
+
+var _ = require('lodash/dist/lodash.underscore');
+var Backbone = require('backbone');
+var should = require('should');
+var backbone_enhanced = require('../lib/main');
+
+describe('backbone_enhanced', function(){
+	describe('has a utilities set that', function(){
+		it('should be able to camelCase convert a space deliniated string', function(){
+			var result = Backbone.EnhancedUtils.toCamelCase("sample string to change");
+			result.should.eql('sampleStringToChange')
+		});
+
+		it('should be able to camelCase convert a dash deliniated string', function(){
+			var result = Backbone.EnhancedUtils.toCamelCase("sample-string-to-change");
+			result.should.eql('sampleStringToChange')
+		});
+
+		it('should be able to camelCase convert a underscore deliniated string', function(){
+			var result = Backbone.EnhancedUtils.toCamelCase("sample_string_to_change");
+			result.should.eql('sampleStringToChange')
+		});
+
+		it('should be able to camelCase convert a dot deliniated string', function(){
+			var result = Backbone.EnhancedUtils.toCamelCase("sample.string.to.change");
+			result.should.eql('sampleStringToChange')
+		});
+	});
+
+	describe('creates accessors which', function(){
+		var TestModel = Backbone.Model.extend({
+			defaults: {
+				"test-attribute": "String Here"
+			}
+		})
+
+		it('should create a getter for an attribute', function(){
+			var result = new TestModel({ "test-attribute": 0 });
+			result.getTestAttribute().should.equal( 0 );
+		});
+
+		it('should create setter for an attribute', function(){
+			var result = new TestModel({ "test-attribute": 0 });
+			result.setTestAttribute( 1 );
+			result.getTestAttribute().should.equal( 1 );
+		});
+
+		it('should create an isAttribute getter for boolean attributes', function(){
+			var result = new TestModel({ "valid-test": false });
+			result.isValidTest().should.equal( false );
+		});
+	});
+});
